cc58014fcf520d219771c1a69c6b8c17c8543308
Craig Franklin <craigjfranklin@gmail.com>
2023-03-03 11:36:40 +1100

    Set up CLI scaffolding for the 'place' command
    I'm not going to bother with CLI end-to-end tests, because they're
    a pain to set up and get working, and that's not really what I
    want to spend time on for this project. So, I'll just be testing
    the app's internal logic rather than input/output.

--- ruby/README.md
+++ ruby/README.md
@@ -5,3 +5,9 @@
 * Install [`rvm`](https://rvm.io/) (or the Ruby version manager of your choice).
 * Install and use the Ruby version in `.ruby-version`.
 * Run `bundle install`
+
+## Running
+
+### CLI
+
+* The base command for the CLI is `ruby toy_robot.rb`, use the `--help` option to get more info about specific commands.
---
+++ ruby/app/interfaces/cli.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'tty-prompt'
+
+# Available commands for the toy_robot CLI
+class CLI
+  COMMAND_PROMPT = 'Select a command to send to the robot'
+  QUIT_MESSAGE = 'Thanks for playing Toy Robot!'
+  PLACE_COMMAND = 'PLACE'
+  QUIT_COMMAND = 'QUIT'
+  COMMANDS = [PLACE_COMMAND, QUIT_COMMAND].freeze
+
+  def initialize
+    @prompt = TTY::Prompt.new
+  end
+
+  def start_game
+    _prompt_command_input
+      .then(&method(:puts))
+  end
+
+  private
+
+  def _prompt_command_input
+    _prompt
+      .select(COMMAND_PROMPT, COMMANDS)
+      .then(&method(:_handle_command))
+  end
+
+  def _prompt
+    @prompt
+  end
+
+  def _handle_command(result)
+    case result
+    when QUIT_COMMAND
+      QUIT_MESSAGE
+    else
+      _prompt_command_input
+    end
+  end
+end
